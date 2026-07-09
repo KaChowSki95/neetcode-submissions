bcc7dc00a103f3f2d47412e3f48daac00d4ebbc1
Melvin Nguyen <40221795+KaChowSki95@users.noreply.github.com>
2026-07-08 20:17:28 -0500

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = []
+        check = {}
+        for i in strs:
+            if "".join(sorted(i)) in check:
+                check["".join(sorted(i))].append(i)
+            else:
+                check["".join(sorted(i))] = [i]
+        for i in check:
+            ans.append(check[i])
+        return ans

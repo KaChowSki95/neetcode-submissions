40763545731d99002670d220f0af318d6702070f
Melvin Nguyen <40221795+KaChowSki95@users.noreply.github.com>
2026-07-09 21:08:07 -0500

    Add: is-anagram - submission-8

---
+++ Data Structures & Algorithms/is-anagram/submission-8.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        checkS = {}
+        for i in s:
+            if i in checkS:
+                checkS[i] += 1
+            else:
+                checkS[i] = 1
+        checkT = {}
+        for i in t:
+            if i in checkT:
+                checkT[i] += 1
+            else:
+                checkT[i] = 1
+        return checkS == checkT
